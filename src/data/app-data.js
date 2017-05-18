09a33b5375d8517ec162c1337e2bd70475dc180c
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-05-18 19:51:28 -0400

    set up appData and pass to app root.

---
+++ src/data/app-data.js
@@ -0,0 +1,16 @@
+export const appData =  {
+    headerData: {
+      image: './avatar.jpg'
+    },
+    cardData: {
+        cardHeader: {
+            title: 'TakeShape CMS',
+            date: 'August 2016 - Present',
+            image: 'path'
+        },
+        cardBody: {
+            title: 'TakeShape CMS',
+            image: 'path'
+        }
+    }
+};
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {appData} from './data/app-data';
 
 ReactDOM.render(
-  <App />,
+  <App appData={appData}/>,
   document.getElementById('root')
 );
