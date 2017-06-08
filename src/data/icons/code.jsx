c6967b683752284bcf1d94c9157abaa5459c2925
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-06-08 17:04:27 -0400

    refactor codeicon html

--- src/data/icons/code.jsx
+++ src/data/icons/code.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CodeIcon(){
     return (
-        <svg version="1.1" x="0px" y="0px" width="40" height="40" viewBox="0 0 512 512">
+        <svg version="1.1" x="0px" y="0px" width="40" height="40" viewBox="0 0 512 512" fill="white">
             <path id="html-code-icon" d="M282.521,137.595h39.674l-90.048,236.811h-39.452L282.521,137.595z M462,272.385L345.031,338.12
 	v-40.762l68.109-38.409l-68.109-38.014v-40.867L462,245.805V272.385z M50,245.805l116.97-65.736v40.867L98.86,258.949l68.109,38.409
 	v40.762L50,272.385V245.805z"/>
