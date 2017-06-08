2f5b7081ca1a956966de3d52183b2656987e0741
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-06-07 23:18:20 -0400

    refactor header grid

--- src/modules/header/header.jsx
+++ src/modules/header/header.jsx
@@ -4,13 +4,19 @@ import  './header.css';
 import FbIcon from '../../data/icons/facebook-box';
 import GmailIcon from '../../data/icons/gmail.jsx';
 import LinkedInIcon from '../../data/icons/linkedin-box';
+import VisibiltySensor from 'react-visibility-sensor'
+
+const onChange = function (isVisible) {
+    console.log('Element is now %s', isVisible ? 'visible' : 'hidden');
+};
 
 export default class Header extends Component {
+
     render() {
         return(
             <div className='headerBody'>
                 <div className='headerContainer row'>
-                    <div className='text five columns offset-by-two'>
+                    <div className='text five columns offset-by-one'>
                         <h3 className='name'>Nick Palmucci</h3>
                         <h5>Software Developer Brooklyn, New York</h5>
                         <div className='icons'>
@@ -19,13 +25,12 @@ export default class Header extends Component {
                             <LinkedInIcon/>
                         </div>
                     </div>
-
-                    <div className='headerImgCont three columns'>
+                    <div className='headerImgCont three columns offset-by-three'>
                         <img src={image} className="headerImage" alt=""/>
                     </div>
                 </div>
+                <VisibiltySensor onChange={onChange}/>
             </div>
-
         )
     }
 }
