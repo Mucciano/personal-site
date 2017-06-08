96615402ade5cd7d36244d1447b85e2b2cd64aae
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-06-07 23:24:58 -0400

    refactor skeleton container layout at app layer

--- src/app/App.js
+++ src/app/App.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import Header from '../modules/header/header';
 import Cards from '../modules/cards/cards';
 
-
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <Header/>
                 <Cards/>
             </div>
