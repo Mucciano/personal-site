f7c4757b526280dc0f5deb6ec4bd1a428162a2e2
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-06-13 17:17:04 -0400

    refactor cards to add headers to enable proper z-index and link functionality

--- src/modules/cards/cards.jsx
+++ src/modules/cards/cards.jsx
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
 import Card from '../card/card';
 import {appData} from '../../data/app-data.js';
+import Header from '../header/header'
+import VisibiltySensor from 'react-visibility-sensor'
+import PostHeader from '../post/post-header';
+import Sticky from 'react-stickynode';
 
 
 export default class Cards extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {head: 'none'};
+    }
+    onChange = (isVisible) => {
+        let display = isVisible ? 'none' : 'flex';
+        if (this.state.head !== display) {
+            this.setState({head: display})
+        }
+    };
 
     render() {
         const cards = [];
@@ -24,9 +38,21 @@ export default class Cards extends Component {
         );
     }
         return (
-            <ul>
+            <div className="">
+                <div className="container">
+                    <VisibiltySensor
+                        onChange={this.onChange}
+                        partialVisibility={true}
+                    >
+                        <Header/>
+                    </VisibiltySensor>
+                </div>
+
+                <Sticky enabled={true} innerZ={99}>
+                    <PostHeader display={this.state.head}/>
+                </Sticky>
                 {cards}
-            </ul>
+            </div>
         )
     }
 }
