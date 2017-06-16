c732a64a122a054fe0ed5280a25aefef4c0222a6
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-06-16 11:47:51 -0400

    add color prop to icons and add index.js

---
+++ src/data/icons/index.js
@@ -0,0 +1,7 @@
+import CloudApp from './cloud-app';
+import Code from './code';
+import Gmail from './gmail';
+import LinkedIn from './linkedin-box';
+import FaceBook from './facebook-box';
+
+export {CloudApp, Code, Gmail, LinkedIn, FaceBook}
