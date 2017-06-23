f5f2e54992aff6e06e514b17a11e3a61943e7c5a
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-06-23 17:39:32 -0400

    refactor post urls in card

--- src/modules/card/card.jsx
+++ src/modules/card/card.jsx
@@ -10,7 +10,7 @@ function Card(data){
                 <CardHeader/>
 
                 <img className='cardImg ten columns offset-by-one' src={data.image} alt=""/>
-                <Link to={`/post/${data.postId}`}>
+                <Link to={`/${data.type}/${data.postId}`}>
                     <div className="cardText row">
                         <h3 className='six columns offset-by-one'>{data.title}</h3>
                         <h4 className="ten columns offset-by-one">{data.body}</h4>
